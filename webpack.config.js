5d691b9a4995c462b653777f411674e447674eb2
awitherow <au.witherow@gmail.com>
2016-08-15 13:42:44 +0200

    basic web pack config

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  entry: './app/main.js',
+  output: {
+    path: './public/',
+    filename: 'index.js',
+  },
+  devServer: {
+    inline: true,
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        query: {
+          presets: ['es2015', 'react'],
+        }
+      }
+    ]
+  }
+}
