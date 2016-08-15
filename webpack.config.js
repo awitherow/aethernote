29b683b98fd623dda0545077f4880da7a5f7e968
awitherow <au.witherow@gmail.com>
2016-08-15 13:49:41 +0200

    adds missing loader

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,7 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
+        loader: 'babel',
         query: {
           presets: ['es2015', 'react'],
         }
