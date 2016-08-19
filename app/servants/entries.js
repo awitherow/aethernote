fbeca48773b66981b362bf6472e157e2bf5bfe70
awitherow <au.witherow@gmail.com>
2016-08-19 16:26:53 +0200

    test

--- app/servants/entries.js
+++ app/servants/entries.js
@@ -1,7 +1,7 @@
 import 'whatwg-fetch';
 
-export const getAllEntries = () => {
-  fetch('/api/entries').then(r => r.json())
-    .then(r => console.log(r))
-    .catch(e => console.log(e, "Booo"));
-};
+// export const getAllEntries = () => {
+//   fetch('/api/entries').then(r => r.json())
+//     .then(r => console.log(r))
+//     .catch(e => console.log(e, "Booo"));
+// };
