082614db1392753f78ebf80842c2129bc6e14348
Austin Witherow <au.witherow@gmail.com>
2016-08-20 20:19:26 +0200

    not using servants at the moment

--- app/servants/entries.js
+++
@@ -1,7 +0,0 @@
-import 'whatwg-fetch';
-
-// export const getAllEntries = () => {
-//   fetch('/api/entries').then(r => r.json())
-//     .then(r => console.log(r))
-//     .catch(e => console.log(e, "Booo"));
-// };
