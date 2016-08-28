64d3c5658c05c2611076c28adc9aceb73880549a
Austin Witherow <au.witherow@gmail.com>
2016-08-28 12:04:59 +0200

    internationalisation will come later

--- app/index.js
+++ app/index.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { IntlProvider } from 'react-intl';
 
 import App from './App';
 
 render(
-  <IntlProvider locale="en">
-    <App />
-  </IntlProvider>
+  <App />
   , document.getElementById('app'));
