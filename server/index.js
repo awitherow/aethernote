6f97a28313cb09e3e792f4b833bd9d8a20199a86
awitherow <au.witherow@gmail.com>
2016-08-31 11:38:57 +0200

    adds react hot loader, not working

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ app.delete('/api/notes/:id', api.removeNote);
 app.use(express.static('public'));
 app.use(favicon('public/favicon.ico'));
 
-app.set('port', (process.env.PORT || 3333));
+app.set('port', (process.env.PORT || 5000));
 
 app.listen(app.get('port'), () => {
   console.log('Node app is running on port', app.get('port'));
