0def7b42b2c881aa641bbd5a7db8c167e2d7971e
Austin Witherow <au.witherow@gmail.com>
2016-09-01 19:19:04 +0200

    adds default routes

--- src/app/routes.js
+++ src/app/routes.js
@@ -1,4 +1,10 @@
 import React from 'react';
 import { Route, IndexRoute } from 'react-router';
-import App from './App';
-import NoteList from './views/NoteList';
+import App from './components/App';
+import NoteList from './components/views/NoteList';
+
+export default (
+  <Route path="/" component={App}>
+    <IndexRoute component={NoteList}/>
+  </Route>
+);
