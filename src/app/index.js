190b26060e73e018d88cfc9dc752eca52aaff1d3
Austin Witherow <au.witherow@gmail.com>
2016-09-03 20:26:04 +0200

    hooks up redux store with react-redux

--- src/app/index.js
+++ src/app/index.js
@@ -1,14 +1,19 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import configureStore from './store/configureStore';
+import { Provider } from 'react-redux';
 import { Router, browserHistory } from 'react-router';
 import routes from './routes';
 import { IntlProvider } from 'react-intl';
 
 import App from './App';
 
+const store = configureStore();
+
 render(
-  <IntlProvider locale="en">
-    <Router history={browserHistory} routes={routes} />
-  </IntlProvider>
-  , document.getElementById('app'));
+  <Provider store={store}>
+    <IntlProvider locale="en">
+      <Router history={browserHistory} routes={routes} />
+    </IntlProvider>
+  </Provider>, document.getElementById('app'));
