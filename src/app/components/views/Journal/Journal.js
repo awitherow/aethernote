9ec150108a3fec6d7e276c1e38f5c64d0824e6b6
Austin Witherow <au.witherow@gmail.com>
2016-09-04 16:48:43 +0200

    not adding stuff on this page.

--- src/app/components/views/Journal/Journal.js
+++ src/app/components/views/Journal/Journal.js
@@ -6,29 +6,8 @@ import * as journalActions from '../../../actions/journalActions';
 import TextInput from '../../elements/TextInput';
 
 class Journal extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      entry: {
-        title: '',
-        type: 'journal'
-      }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(e) {
-    const entry = this.state.entry;
-    entry.title = e.target.value;
-    this.setState({ entry });
-  }
-
-  onClickSave(e) {
-    e.preventDefault();
-    this.props.actions.createEntry(this.state.entry);
+  constructor(props, context) {
+    super(props, context);
   }
 
   journalEntryRow(entry, index) {
@@ -42,25 +21,11 @@ class Journal extends Component {
   render() {
     return (
       <div className="journal">
+      
         <h1>Journal Entries</h1>
 
         {this.props.journal.map(this.journalEntryRow)}
 
-        <h2>Add Journal Entry</h2>
-        <form className="add-journal__form">
-          <TextInput
-            id="note"
-            label="Awaiting changes..."
-            defaultValue={this.state.entry.title}
-            onChange={this.onTitleChange}
-            />
-
-            <input
-              type="submit"
-              value="save"
-              onClick={this.onClickSave}
-              />
-        </form>
       </div>
     );
   }
