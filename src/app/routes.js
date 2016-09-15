adf42d9ca75b63f07d0a2e50c2ec38ee1c542eeb
Austin Witherow <au.witherow@gmail.com>
2016-09-15 20:31:08 +0200

    adds journal entry route

--- src/app/routes.js
+++ src/app/routes.js
@@ -3,10 +3,12 @@ import { Route, IndexRoute } from 'react-router';
 import App from './App';
 import NoteList from './components/views/NoteList';
 import Journal from './components/views/Journal';
+import JournalEntry from './components/views/Journal/Entry';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={NoteList}/>
     <Route path="/journal" component={Journal}/>
+    <Route path="/journal-entry" component={JournalEntry}/>
   </Route>
 );
