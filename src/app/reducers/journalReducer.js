a51b908aaa91f7dea2c138142d648c49fb99b697
Austin Witherow <au.witherow@gmail.com>
2016-09-15 21:26:51 +0200

    adds initial state for journal.

--- src/app/reducers/journalReducer.js
+++ src/app/reducers/journalReducer.js
@@ -1,6 +1,7 @@
 import * as types from '../actions/actionTypes';
+import initialState from './initialState';
 
-export default function(state = [], action) {
+export default function(state = initialState.journal, action) {
   switch(action.type) {
     case types.CREATE_ENTRY:
       return [
