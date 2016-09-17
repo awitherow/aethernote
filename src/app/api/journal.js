d23f92aca98a7c35934c4f5a5ae6bc3ff6cc731a
Austin Witherow <au.witherow@gmail.com>
2016-09-17 12:11:12 +0200

    proper api call

--- src/app/api/journal.js
+++ src/app/api/journal.js
@@ -3,19 +3,18 @@ function getEntries(cb) {
     .then(r => r.json())
     .then(res => {
       cb(res.data);
-    })
-    .catch(e => console.log(e));
+    }).catch(e => console.log(e));
 }
 
 function saveEntry(entry, cb) {
-  fetch('/api/saveJournalEntry', {
+  fetch('/api/journalEntries', {
     method: 'POST',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
     body: JSON.stringify(entry)
-  }).then(cb);
+  }).then(cb).catch(e => console.log(e));
 }
 
 export {
