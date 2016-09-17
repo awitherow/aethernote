c171312529e2291c831a4bfd73daa6e9eca2aedf
Austin Witherow <au.witherow@gmail.com>
2016-09-17 12:09:29 +0200

    removes unused CREATE_ENTRY

--- src/app/actions/journalActions.js
+++ src/app/actions/journalActions.js
@@ -1,13 +1,6 @@
 import * as types from './actionTypes';
 import * as journalApi from '../api/journal';
 
-export function createEntry(entry) {
-  return {
-    type: types.CREATE_ENTRY,
-    entry
-  };
-}
-
 export function loadEntriesSuccess(entries) {
   return {
     type: types.LOAD_ENTRIES_SUCCESS,
