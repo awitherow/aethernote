5a15b07f368d542d7c82f61feb9bd8f4733ef658
Austin Witherow <au.witherow@gmail.com>
2016-09-17 12:09:51 +0200

    adds proper reducer for saving entry

--- src/app/reducers/journalReducer.js
+++ src/app/reducers/journalReducer.js
@@ -3,13 +3,13 @@ import initialState from './initialState';
 
 export default function(state = initialState.journal, action) {
   switch(action.type) {
-    case types.CREATE_ENTRY:
+    case types.LOAD_ENTRIES_SUCCESS:
+      return action.entries;
+    case types.SAVE_ENTRIES_SUCCESS:
       return [
         ...state,
         Object.assign({}, action.entry)
       ];
-    case types.LOAD_ENTRIES_SUCCESS:
-      return action.entries;
     default:
       return state;
   }
