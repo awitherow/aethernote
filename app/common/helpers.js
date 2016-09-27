b1c6aabcc019b54550d28d2e050fa1192910d9fc
Austin Witherow <au.witherow@gmail.com>
2016-09-27 23:43:23 +0100

    readability

--- app/common/helpers.js
+++ app/common/helpers.js
@@ -2,5 +2,5 @@ import Remarkable from 'remarkable';
 
 export function convertToMarkdown(text) {
   let md = new Remarkable();
-  return {__html: md.render(text)};
+  return { __html: md.render(text) };
 }
