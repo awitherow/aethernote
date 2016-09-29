6998768120e3c27e5567cdea1a9fad16a68d97ab
Austin Witherow <au.witherow@gmail.com>
2016-09-29 16:51:34 +0100

    initialises new store and db connection

---
+++ app/api/store.js
@@ -0,0 +1,12 @@
+import syncedDb from 'synceddb-client';
+
+const stores = {
+  notes: [],
+};
+
+export const db = syncedDb.open({
+  name: 'Aether',
+  version: 1,
+  stores,
+  remote: process.env.DATABASE_URL,
+});
