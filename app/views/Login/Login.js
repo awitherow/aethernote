5144c7076a06c6393125aa74265fde899f7db1ea
Austin Witherow <au.witherow@gmail.com>
2016-09-30 16:34:31 +0200

    adds simple failure checker
    TODO: greater than or equal to 3 failures -> …

--- app/views/Login/Login.js
+++ app/views/Login/Login.js
@@ -5,9 +5,27 @@ import checkAuthentication from './common/auth';
 export default class Login extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      failureAttempts: 0,
+      userId: '',
+      userKey: '',
+    };
+    this.authenticateLoginAttempt = this.authenticateLoginAttempt.bind(this);
   }
 
   authenticateLoginAttempt() {
+    if (!checkAuthentication) {
+      const { failureAttempts } = this.state;
+
+      if (failureAttempts >= 3) {
+        // set locked cookie.
+        // redirect to some messed up website.
+      } else {
+        this.setState({
+          failureAttempts: failureAttempts + 1,
+        });
+      }
+    }
   }
 
   render() {
@@ -21,6 +39,10 @@ export default class Login extends Component {
 
 Login.propTypes = {
   test: PropTypes.string.isRequired,
+};
+
+Login.contextTypes = {
+  update: PropTypes.func.isRequired,
 };
 
 export default Login;
