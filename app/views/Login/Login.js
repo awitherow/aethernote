54ea7cddc512f5fa7ce6078f936abbd7e50148d2
Austin Witherow <au.witherow@gmail.com>
2016-10-01 10:33:13 +0200

    creates basic login template

--- app/views/Login/Login.js
+++ app/views/Login/Login.js
@@ -2,6 +2,8 @@ import React, { Component, PropTypes } from 'react';
 
 import checkAuthentication from '../../common/auth';
 
+import TextInput from '../../elements/TextInput';
+
 class Login extends Component {
   constructor(props) {
     super(props);
@@ -13,8 +15,10 @@ class Login extends Component {
     this.authenticateLoginAttempt = this.authenticateLoginAttempt.bind(this);
   }
 
-  authenticateLoginAttempt() {
-    if (!checkAuthentication) {
+  authenticateLoginAttempt(e) {
+    e.preventDefault();
+    const { userId, userKey } = this.state;
+    if (!checkAuthentication({ userId, userKey })) {
       const { failureAttempts } = this.state;
 
       if (failureAttempts >= 3) {
@@ -25,13 +29,31 @@ class Login extends Component {
           failureAttempts: failureAttempts + 1,
         });
       }
+    } else {
+      this.context.update('auth', true);
     }
   }
 
   render() {
+    const { userId, userKey } = this.state;
     return (
       <form onSubmit={this.authenticateLoginAttempt}>
-
+        <TextInput
+          id="userId"
+          label="Username"
+          defaultValue={userId}
+          onChange={(e) => this.setState({ userId: e.target.value })}
+          />
+        <fieldset>
+          <label htmlFor="userKey">Password</label>
+          <input
+            id="userKey"
+            type="password"
+            defaultValue={userKey}
+            onChange={(e) => this.setState({ userKey: e.target.value })}
+            />
+        </fieldset>
+        <button>Submit</button>
       </form>
     );
   }
