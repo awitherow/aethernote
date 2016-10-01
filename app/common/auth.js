b34107aa1dd83a1441d063cda00c2d3efb7a7e62
Austin Witherow <au.witherow@gmail.com>
2016-10-01 11:08:11 +0200

    delete common/auth

--- app/common/auth.js
+++
@@ -1,9 +0,0 @@
-export function authenticateLogin({ userId, userKey }) {
-  const validUserId = "";
-  const validUserKey = "";
-  if (userId === validUserId && userKey === validUserKey) {
-    return true;
-  } else {
-    return false;
-  }
-}
