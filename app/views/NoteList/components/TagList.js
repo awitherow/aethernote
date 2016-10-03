dba752e0a10b66943ece5683206bef8a895902a1
Austin Witherow <au.witherow@gmail.com>
2016-10-03 11:41:43 +0200

    tagList -> component with state to handle tag input

--- app/views/NoteList/components/TagList.js
+++ app/views/NoteList/components/TagList.js
@@ -1,24 +1,53 @@
-import React, { PropTypes } from 'react';
+import React, { Component, PropTypes } from 'react';
 import TextInput from '../../../elements/TextInput';
 
-const TagList = ({
-  handleChange,
-  tags,
-}) => {
-  return (
-    <ul>
-      {tags.map(tag => {
-        return <li>{tag}</li>;
-      })}
-      <TextInput
-        id="tag"
-        label="tag"
-        defaultValue="Add new tag"
-        onChange={(e) => this.handleChange('tag', e.target.value)}
-        />
-    </ul>
-  );
-};
+export class TagList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tagInput: '',
+      tags: [],
+    };
+
+    this.addTag = this.addTag.bind(this);
+  }
+
+  captureTag(tagInput, change) {
+    this.setState({ tagInput: change });
+  }
+
+  addTag(e) {
+    e.preventDefault();
+    const { tags, tagInput } = this.state;
+    let tagList = tags;
+    tagList.push(tagInput);
+    this.props.handleChange('tag', tagInput);
+    this.setState({
+      tagInput: '',
+      tags: tagList,
+    });
+  }
+
+  render() {
+    const tagList = this.props.tags.concat(this.state.tags);
+    return (
+      <ul>
+        {tagList.map(tag => {
+          return <li key={tagList.indexOf(tag)}>{tag}</li>;
+        })}
+        <li>
+          <TextInput
+            id="tag"
+            label="tag"
+            defaultValue="New tag..."
+            onChange={(e) => this.captureTag('tagInput', e.target.value)}
+            />
+          <button onClick={this.addTag}>Add Tag</button>
+        </li>
+      </ul>
+    );
+  }
+}
 
 TagList.propTypes = {
   handleChange: PropTypes.func.isRequired,
