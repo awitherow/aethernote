42824902cd349b22131f85071f8420b475cbfde4
Austin Witherow <au.witherow@gmail.com>
2016-10-03 17:21:34 +0200

    adds dropdown element

---
+++ app/elements/Dropdown/dropdown.js
@@ -0,0 +1,29 @@
+import React, { PropTypes } from 'react';
+
+const Dropdown = ({
+  id,
+  label,
+  options,
+  handleChange,
+}) => {
+  const fieldsetClasses = `${id}-field`;
+  return (
+    <fieldset className={fieldsetClasses}>
+      <label htmlFor={id}>{label}</label>
+      <select id={id} name="select" onChange={handleChange}>
+        {options.map(option => {
+          return <option value={option}>{option}</option>;
+        })}
+      </select>
+    </fieldset>
+  );
+};
+
+Dropdown.propTypes = {
+  id: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  options: PropTypes.array.isRequired,
+  handleChange: PropTypes.func.isRequired,
+};
+
+export default Dropdown;
