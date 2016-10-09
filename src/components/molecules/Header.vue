4e94c31cf3c87046dacbacb15a5cee3cbdc0d682
Austin Witherow <au.witherow@gmail.com>
2016-10-09 15:31:16 +0200

    adds basic styling

--- src/components/molecules/Header.vue
+++ src/components/molecules/Header.vue
@@ -12,6 +12,15 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+#header {
+  background: black;
+}
 
+h1 {
+  font-size: 24px;
+  margin: 0;
+  color: white;
+  padding: 15px 30px;
+}
 </style>
