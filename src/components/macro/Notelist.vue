160fd4eab8f2efac1222f6fb475a5cc97bebded1
Austin Witherow <au.witherow@gmail.com>
2016-10-10 09:28:44 +0200

    adds basic notes area without styling

--- src/components/macro/Notelist.vue
+++ src/components/macro/Notelist.vue
@@ -1,5 +1,19 @@
 <template lang=pug>
   #note-list(v-show="!loading")
+    #add-note
+      fieldset
+        label(for="content")
+        input(
+          name="prio"
+          v-model="newNote.content"
+        )
+      fieldset
+        label(for="prio")
+        input(
+          name="prio"
+          type="checkbox"
+          v-model="newNote.prio"
+        )
     ul
       li(v-for="note in notes") {{ note.title }}
 </template>
@@ -7,7 +21,15 @@
 <script>
 export default {
   name: 'Notelist',
-  props: ['notes', 'loading']
+  props: ['notes', 'loading'],
+  data: () => ({
+    newNote: {
+      content: '',
+      prio: false,
+      status: 'inbox',
+      context: 'personal'
+    }
+  })
 }
 </script>
 
