47b7efa47745ab9a994ef559a673aece11f89d85
Austin Witherow <au.witherow@gmail.com>
2016-10-24 22:37:51 +0200

    adds reusable Dropdown component

---
+++ src/components/elements/Dropdown.vue
@@ -0,0 +1,33 @@
+<template>
+  <fieldset>
+    <label :for="id">{{label}}</label>
+    <select
+      :id="id"
+      name="select"
+      @change="handleChange"
+      v-model="currentOption">
+        <option v-for="option in options">{{option}}</option>
+    </select>
+  </fieldset>
+</template>
+
+<script>
+export default {
+  name: 'Dropdown',
+  props: ['id', 'selected', 'options', 'label', 'onChange'],
+  computed: {
+    currentOption () {
+      return this.selected
+    }
+  },
+  methods: {
+    handleChange (e) {
+      this.onChange(this.id, e.target.value)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '../../common/styles/index.scss';
+</style>
