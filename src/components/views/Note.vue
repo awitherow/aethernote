22d4383c846082db5e52089b856363de22d0b357
Austin Witherow <au.witherow@gmail.com>
2016-10-25 18:45:46 +0200

    adds basic ass template for note

---
+++ src/components/views/Note.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    Hello! {{ $route.params.id }}
+  </div>
+</template>
+
+<script>
+let local = {}
+
+export default {
+  name: 'Note',
+  data: () => (local)
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
