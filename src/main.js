02f0ceb14a5dea356cd066e221bcb67d6abc9687
Austin Witherow <au.witherow@gmail.com>
2016-10-25 20:55:24 +0200

    beautifully synchronises router and store.

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import Aether from './Aether'
-
+import { sync } from 'vuex-router-sync'
 import store from './store'
 import router from './router'
+
+sync(store, router)
 
 /* eslint-disable no-new */
 new Vue({
