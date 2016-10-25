8d684486a8a70c585dbb963dbed57cc08dda32de
Austin Witherow <au.witherow@gmail.com>
2016-10-25 20:56:45 +0200

    selects activeNote for editing.

--- src/components/views/Note.vue
+++ src/components/views/Note.vue
@@ -1,15 +1,33 @@
 <template>
-  <div>
-    Hello! {{ $route.params.id }}
+  <div v-if="activeNote">
+    Hello! {{ activeNote.id }}
   </div>
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
+
 let local = {}
 
 export default {
   name: 'Note',
-  data: () => (local)
+  data: () => (local),
+  created () {
+    if (!this.notes.length) {
+      this.loadNotes()
+    }
+  },
+  methods: {
+    ...mapActions(['loadNotes'])
+  },
+  computed: {
+    ...mapState({
+      loading: state => state.loading,
+      notes: state => state.notes,
+      activeNote: state => state.notes.filter(note =>
+        note.id === parseInt(state.route.params.id))[0]
+    })
+  }
 }
 </script>
 
