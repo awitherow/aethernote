82dc279853f1b26a93586dcf16b6685f62775a5f
awitherow <au.witherow@gmail.com>
2016-10-31 13:54:24 +0100

    adds computed type

--- src/components/elements/TextInput.vue
+++ src/components/elements/TextInput.vue
@@ -1,13 +1,16 @@
 <template>
   <fieldset :id="id + '-fieldset'">
     <label :for="id">{{label}}</label>
-    <input :id="id" :value="value" @input="onInput" />
+    <input :type="computedType" :id="id" :value="value" @input="onInput" />
   </fieldset>
 </template>
 
 <script>
 export default {
   name: 'TextInput',
-  props: ['id', 'value', 'label', 'onInput']
+  props: ['id', 'value', 'type', 'label', 'onInput'],
+  computed: {
+    computedType: () => this.type ? this.type : 'text'
+  }
 }
 </script>
