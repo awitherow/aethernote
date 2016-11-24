6df4cdae3ff0834a915d66239bae197a72ecb0ec
awitherow <au.witherow@gmail.com>
2016-11-24 21:36:46 +0100

    only show entries from today.

--- app/views/Journal/index.js
+++ app/views/Journal/index.js
@@ -11,13 +11,19 @@ export default class Journal extends Component {
     getNotes: PropTypes.func.isRequired,
   }
 
+  getEntriesFromSelectedDay = (dateCreated) => {
+    const today = new Date().setHours(0, 0, 0, 0)
+    const entryDay = new Date(dateCreated).setHours(0, 0, 0, 0)
+    return today === entryDay
+  }
+
   render () {
     return (
       <div>
         <ul>
-          {this.props.notes.map(journalEntry =>
-            <li>{journalEntry.content}</li>
-          )}
+          {this.props.notes
+            .filter(e => this.getEntriesFromSelectedDay(e.created))
+            .map(e => <li key={e.created}>{e.content}</li>)}
         </ul>
 
         <AddNote
