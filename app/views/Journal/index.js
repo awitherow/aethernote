6a3707e64a6b3f53928176854ec5ff2d64c9f9c0
awitherow <au.witherow@gmail.com>
2016-11-24 21:14:55 +0100

    add basic ass journal view

--- app/views/Journal/index.js
+++ app/views/Journal/index.js
@@ -1,7 +1,30 @@
-import React, { Component } from 'react'
+import React, { Component, PropTypes } from 'react'
+import AddNote from '../../elements/AddNote'
 
 export default class Journal extends Component {
+  static propTypes = {
+    notes: PropTypes.array.isRequired,
+  }
+
+  static contextTypes = {
+    update: PropTypes.func.isRequired,
+    getNotes: PropTypes.func.isRequired,
+  }
+
   render () {
-    return <p></p>
+    return (
+      <div>
+        <ul>
+          {this.props.notes.map(journalEntry =>
+            <li>{journalEntry.content}</li>
+          )}
+        </ul>
+
+        <AddNote
+          getNotes={this.context.getNotes}
+          type="journal"
+          />
+      </div>
+    )
   }
 }
