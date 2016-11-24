7501c180910db81688421de755470937af9adefc
awitherow <au.witherow@gmail.com>
2016-11-24 19:11:42 +0100

    index calling app -> index

--- app/index.js
+++ app/index.js
@@ -1,8 +1,58 @@
-import React from 'react'
 import { render } from 'react-dom'
 
-import App from './App'
+import './common/styles.scss'
+import React, { Component, PropTypes } from 'react'
 
-render(
-  <App />
-  , document.getElementById('app'))
+import Overlay from './elements/Overlay'
+import Header from './elements/Header'
+
+import NoteList from './views/NoteList'
+import Login from './views/Login'
+
+class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      loading: false,
+      authenticated: false,
+    }
+  }
+
+  getChildContext() {
+    return {
+      update: this.update.bind(this),
+    }
+  }
+
+  update(action, data) {
+    let payload
+    switch(action) {
+      case 'loading': payload = { loading: data }; break
+      case 'auth': payload = { authenticated: data }; break
+    }
+    this.setState(payload)
+  }
+
+  render() {
+    const { loading, authenticated } = this.state
+
+    return !authenticated ? <Login /> : (
+      <div className="aether">
+
+        { loading ? <Overlay type="loading" /> : null }
+
+        <Header />
+
+        <NoteList />
+
+      </div>
+    )
+  }
+}
+
+App.childContextTypes = {
+  update: PropTypes.func,
+}
+
+
+render(<App />, document.getElementById('app'))
