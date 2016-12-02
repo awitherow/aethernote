84ac089179af9392fdcd7fefc0eb739a648d4f45
awitherow <au.witherow@gmail.com>
2016-12-02 11:28:40 +0100

    ternary because

--- app/index.js
+++ app/index.js
@@ -16,12 +16,7 @@ import Journal from './views/Journal'
 import Login from './views/Login'
 
 function checkAuthentication() {
-  if (process.env.NODE_ENV === 'development') {
-    return true
-  } else {
-    // TODO: check cookies
-    return false
-  }
+  return process.env.NODE_ENV === 'development' ? true : false
 }
 
 const initialEditorState = {
