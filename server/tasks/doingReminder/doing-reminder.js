4369e50bb85a8314a77a608336428f5b0f3f06e4
awitherow <au.witherow@gmail.com>
2016-12-06 12:02:39 +0100

    refactor doingReminders email

--- server/tasks/doingReminder/doing-reminder.js
+++ server/tasks/doingReminder/doing-reminder.js
@@ -1,6 +1,8 @@
 import promiseLib from 'bluebird'
 import pg from 'pg-promise'
 
+import { doingReminder } from './templates/templates'
+
 const pgp = pg({ promiseLib })
 
 pgp.pg.defaults.ssl = true
@@ -11,26 +13,23 @@ function sendMail() {
   let GMAIL_PASS = process.env.GMAIL_PASS
   let PRIVATE_EMAIL = process.env.PRIVATE_EMAIL
 
-  let titles = []
-  getTasks('doing')
-    .then(tasks => tasks.map(task => titles.push(task.title)))
-    .then(() => {
-      let nodemailer = require('nodemailer')
-      let transporter = nodemailer.createTransport(
-        `smtps://${GMAIL_USER}%40gmail.com:${GMAIL_PASS}@smtp.gmail.com`
-      )
+  getTasks('doing').then(tasks => {
+    let titles = tasks.map(task => task.title)
+    let nodemailer = require('nodemailer')
+    let transporter = nodemailer.createTransport(
+      `smtps://${GMAIL_USER}%40gmail.com:${GMAIL_PASS}@smtp.gmail.com`
+    )
 
-      transporter.sendMail({
-        from: GMAIL_USER,
-        to: PRIVATE_EMAIL,
-        subject: 'Daily Reminder',
-        text: `${titles.join('\n')}`,
-      }, (error, info) => {
-        if (error) return console.log(error)
-        console.log('Message sent: ' + info.response)
-      })
-    }
-  )
+    transporter.sendMail({
+      from: GMAIL_USER,
+      to: PRIVATE_EMAIL,
+      subject: 'Daily Reminder',
+      text: doingReminder(titles),
+    }, (error, info) => {
+      if (error) return console.log(error)
+      console.log('Message sent: ' + info.response)
+    })
+  })
 }
 
 function getTasks(category) {
