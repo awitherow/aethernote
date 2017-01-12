160e9c6be64db89890b276d049b5b846f3749e39
awitherow <au.witherow@gmail.com>
2017-01-12 21:02:08 +0100

    feat(api): add user connections

---
+++ server/queries/user.js
@@ -0,0 +1,26 @@
+import bcrypt from 'bcrypt'
+import db from '../db'
+
+export const getUser = (req, res, next) =>
+    db.one(`select * from entries where id = ${req.params.id}`)
+        .then(data => res.status(200).json({
+            status: 'success',
+            data,
+            message: 'Retrieved user',
+        })).catch(err => next(err))
+
+export const createUser = (req, res, next) => {
+    const { username, password } = req.body
+    const salt = bcrypt.genSaltSync();
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    db.none('insert into users(username, password) values( ${username}, ${password})', {
+        username,
+        password: hash,
+    }).then(() => 
+        res.status(200).json({
+            status: 'success',
+            message: 'Inserted one entry',
+        })
+    ).catch(err => next(err))
+}
