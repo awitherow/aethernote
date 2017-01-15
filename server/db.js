67a1a58923153d47e95ead07f70e45a0b5ac4c99
awitherow <au.witherow@gmail.com>
2017-01-15 18:09:11 +0100

    refactor(db): remove unnecessary promise lib

--- server/db.js
+++ server/db.js
@@ -1,10 +1,6 @@
-import promiseLib from 'bluebird'
-import pg from 'pg-promise'
-
-const pgp = pg({
-  promiseLib,
-})
+const pgp = require('pg-promise')()
 
 pgp.pg.defaults.ssl = true
 const db = pgp(process.env.DATABASE_URL)
-module.exports = db
+
+export default db
