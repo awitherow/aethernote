d1e657155a854687a141d3ebe34854628b91e608
awitherow <au.witherow@gmail.com>
2017-01-18 22:24:26 +0100

    chore(hmr): split application to load with hotRenderer

---
+++ app/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+import store from './redux/store'
+
+import { Router, hashHistory } from 'react-router'
+import routes from './routes'
+
+let s = createStore(store,
+  process.env.NODE_ENV === 'development' ? (
+    window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    window.__REDUX_DEVTOOLS_EXTENSION__()
+  ) : null
+)
+
+const App = () => (
+  <Provider store={s}>
+    <Router history={hashHistory} routes={routes} />
+  </Provider>
+)
+
+
+export default App
--- app/index.js
+++ app/index.js
@@ -2,23 +2,27 @@ import './lib/styles.css'
 import React from 'react'
 import { render } from 'react-dom'
 
-import { Provider } from 'react-redux'
-import { createStore } from 'redux'
-import store from './redux/store'
+import App from './App'
 
-import { Router, hashHistory } from 'react-router'
-import routes from './routes'
+if (process.env.NODE_ENV === 'development') {
+  const { AppContainer } = require('react-hot-loader')
+  const hotRender = (Component) => {
+    render(
+      <AppContainer>
+        <Component/>
+      </AppContainer>,
+      document.getElementById('root')
+    )
+  }
 
-let s = createStore(store,
-  process.env.NODE_ENV === 'development' ? (
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()
-  ) : null
-)
+  hotRender(App)
 
-render(
-  <Provider store={s}>
-    <Router history={hashHistory} routes={routes} />
-  </Provider>,
-  document.getElementById('app')
-)
+  if (module.hot) {
+    module.hot.accept('./App', () => {
+      const NewApp = require('./App').default
+      hotRender(NewApp)
+    })
+  }
+} else {
+  render(App, document.getElementById('app'))
+}
