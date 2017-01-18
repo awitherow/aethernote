f0f4bdfc625661428a2c08a84631b63e96c1b2b2
awitherow <au.witherow@gmail.com>
2017-01-18 22:21:50 +0100

    chore(webpack): add webpack dev and prod splits

--- webpack.config.js
+++ webpack.config.js
@@ -1,69 +1,117 @@
-var webpack = require('webpack')
-var CompressionPlugin = require('compression-webpack-plugin')
-var ExtractTextPlugin = require('extract-text-webpack-plugin')
+const { resolve } = require('path')
+const webpack = require('webpack')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const CompressionPlugin = require('compression-webpack-plugin')
 
-var nodeEnv = process.env.NODE_ENV
+const nodeEnv = process.env.NODE_ENV
 
-var config = {
-  devtool: nodeEnv === 'production' ? 'cheap-module-source-map' : 'inline-eval-cheap-source-map',
-  entry: [
-    'promise-polyfill',
-    './app/index.js',
-  ],
-  output: {
-    path: 'public',
-    filename: 'index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader'],
-      },
-      {
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract({
-          fallbackLoader: "style-loader",
-          loader: "css-loader",
-          publicPath: "/public",
-        }),
-      },
+module.exports = function(env) {
+  return env === 'dev' ? {
+    devtool: 'cheap-module-source-map',
+    entry: [
+      'promise-polyfill',
+      './app/index.js',
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.css'],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
-    }),
-    new ExtractTextPlugin({
-      filename: "bundle.css",
-      disable: false,
-      allChunks: true,
-    }),
-  ],
+    output: {
+      path: 'public',
+      filename: 'index.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: "style-loader",
+            loader: "css-loader",
+            publicPath: "/public",
+          }),
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.css'],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+      }),
+      new ExtractTextPlugin({
+        filename: "bundle.css",
+        disable: false,
+        allChunks: true,
+      }),
+      new webpack.optimize.UglifyJsPlugin({
+        comments: false,
+      }),
+      new webpack.LoaderOptionsPlugin({
+        minimize: true,
+      }),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ],
+  } : {
+    devtool: 'inline-source-map',
+    entry: [
+      'react-hot-loader/patch',
+      'webpack-dev-server/client?http://localhost:8080',
+      'webpack/hot/only-dev-server',
+      './app/index.js',
+    ],
+    output: {
+      filename: 'index.js',
+      path: 'public',
+      publicPath: '/',
+    },
+    devServer: {
+      hot: true,
+      contentBase: resolve(__dirname, 'dist'),
+      publicPath: '/',
+    },
+    proxy: {
+      "/api": "http://localhost:3333",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: ['babel-loader'],
+        },
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: "style-loader",
+            loader: "css-loader",
+            publicPath: "/public",
+          }),
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.css'],
+    },
+    plugins: [
+      new webpack.HotModuleReplacementPlugin(),
+      new webpack.NamedModulesPlugin(),
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
+      }),
+      new ExtractTextPlugin({
+        filename: "bundle.css",
+        disable: false,
+        allChunks: true,
+      }),
+    ],
+  }
 }
-
-if (nodeEnv === 'production') {
-  config.plugins = config.plugins.concat([
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-    }),
-    new webpack.LoaderOptionsPlugin({
-      minimize: true,
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  ])
-}
-
-module.exports = config
