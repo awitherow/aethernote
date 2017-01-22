a5d05fb697fb3481488d823478b4ce0632d7905d
awitherow <au.witherow@gmail.com>
2017-01-22 21:09:53 +0100

    chore(App): reorganize app structure

---
+++ app/routes.js
@@ -0,0 +1,32 @@
+import CMS from './containers/CMS'
+import Portal from './containers/Portal'
+import Planner from './containers/Planner'
+import Aether from './Aether'
+import { isUserAuthenticated, deauthenticateUser } from './api/security'
+
+export default {
+  component: Aether,
+  childRoutes: [
+    {
+      path: '/',
+      getComponent: (location, callback) => {
+        if (isUserAuthenticated()) {
+          callback(null, CMS)
+        } else {
+          callback(null, Portal)
+        }
+      },
+    },
+    {
+      path: '/portal',
+      component: Portal,
+    },
+    {
+      path: '/logout',
+      onEnter: (nextState, replace) => {
+        deauthenticateUser()
+        replace('/')
+      },
+    },
+  ],
+}
