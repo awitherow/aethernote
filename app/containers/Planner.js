8ae692a6ac64d83c91509027ed0ffd2e9f92ff04
awitherow <au.witherow@gmail.com>
2017-01-23 20:07:05 +0100

    chore(Planner): set up page template

--- app/containers/Planner.js
+++ app/containers/Planner.js
@@ -1,9 +1,33 @@
+import './Planner.scss'
 import React, { Component } from 'react'
 
+import { Nav, NavItem } from 'react-bootstrap'
+
 class Planner extends Component {
+  state = {
+    activeDay: 2,
+  }
+
   render() {
     return (
-      <p>Planner</p>
+      <div id="planner-page">
+        <div className="add-button-container">
+          <button>Add Item</button>
+        </div>
+        <Nav
+          bsStyle="pills"
+          justified
+          activeKey={this.state.activeDay}
+          onSelect={(e) => console.log(e)}
+          >
+          <NavItem eventKey={1}>Yesterday</NavItem>
+          <NavItem eventKey={2}>Today</NavItem>
+          <NavItem eventKey={3}>Tomorrow</NavItem>
+        </Nav>
+        <div>
+
+        </div>
+      </div>
     )
   }
 }
