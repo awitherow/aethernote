1e993ba8baa74333ce317e29e5c5d721ecaeb6d4
awitherow <au.witherow@gmail.com>
2017-01-23 20:34:51 +0100

    fix(webpack): properly use env variable

--- webpack.config.js
+++ webpack.config.js
@@ -1,62 +1,62 @@
 var webpack = require('webpack')
 var CompressionPlugin = require('compression-webpack-plugin')
 
-var nodeEnv = process.env.NODE_ENV
-
-var config = {
-  devtool: nodeEnv === 'production' ? 'cheap-module-source-map' : 'inline-eval-cheap-source-map',
-  entry: [
-    'promise-polyfill',
-    'whatwg-fetch',
-    './app/index.js',
-  ],
-  output: {
-    path: 'public',
-    filename: 'index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: [
-          "style-loader",
-          "css-loader",
-          "sass-loader",
-        ],
-      },
+module.exports = function(env) {
+  var config = {
+    devtool: env === 'production' ? 'cheap-module-source-map' : 'inline-eval-cheap-source-map',
+    entry: [
+      'promise-polyfill',
+      'whatwg-fetch',
+      './app/index.js',
     ],
-  },
-  resolve: {
-    extensions: ['.js', '.css'],
-  },
-  plugins: [
-    new webpack.DefinePlugin({
-      'process.env': { NODE_ENV: JSON.stringify(nodeEnv) },
-    }),
-  ],
-}
+    output: {
+      path: 'public',
+      filename: 'index.js',
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: 'babel-loader',
+        },
+        {
+          test: /\.scss$/,
+          use: [
+            "style-loader",
+            "css-loader",
+            "sass-loader",
+          ],
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.js', '.css'],
+    },
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': { NODE_ENV: JSON.stringify(env) },
+      }),
+    ],
+  }
 
-if (nodeEnv === 'production') {
-  config.plugins = config.plugins.concat([
-    new webpack.optimize.DedupePlugin(),
-    new webpack.optimize.OccurrenceOrderPlugin(true),
-    new webpack.optimize.UglifyJsPlugin({
-      comments: false,
-    }),
-    new webpack.optimize.AggressiveMergingPlugin(),
-    new CompressionPlugin({
-      asset: "[path].gz[query]",
-      algorithm: "gzip",
-      test: /\.js$|\.css$|\.html$/,
-      threshold: 10240,
-      minRatio: 0.8,
-    }),
-  ])
-}
+  if (env === 'production') {
+    config.plugins.concat([
+      new webpack.optimize.DedupePlugin(),
+      new webpack.optimize.OccurrenceOrderPlugin(true),
+      new webpack.optimize.UglifyJsPlugin({
+        comments: false,
+      }),
+      new webpack.optimize.AggressiveMergingPlugin(),
+      new CompressionPlugin({
+        asset: "[path].gz[query]",
+        algorithm: "gzip",
+        test: /\.js$|\.css$|\.html$/,
+        threshold: 10240,
+        minRatio: 0.8,
+      }),
+    ])
+  }
 
-module.exports = config
+  return config
+}
