f52650fda2edbf5400b4c8d64aa5444710bd51c3
awitherow <au.witherow@gmail.com>
2017-01-26 23:17:02 +0100

    feat(hmr): add hmr

--- app/index.js
+++ app/index.js
@@ -9,6 +9,8 @@ import store from './redux/store'
 import { Router, hashHistory } from 'react-router'
 import routes from './routes'
 
+const rootEl = document.getElementById('app')
+
 let s = createStore(store,
   process.env.NODE_ENV === 'development' ? (
     window.__REDUX_DEVTOOLS_EXTENSION__ &&
@@ -16,9 +18,32 @@ let s = createStore(store,
   ) : null
 )
 
-render(
+const App = (
   <Provider store={s}>
     <Router history={hashHistory} routes={routes} />
-  </Provider>,
-  document.getElementById('app')
+  </Provider>
 )
+
+if (process.env.NODE_ENV === 'dev') {
+  const { AppContainer } = require('react-hot-loader')
+  const hotRender = (Component) => {
+    render(
+      <AppContainer>
+        <Component />
+      </AppContainer>,
+      rootEl,
+    )
+  }
+
+  hotRender(App)
+
+  if (module.hot) {
+    module.hot.accept('./containers/Aether', () => {
+      module.hot.accept('./redux/store', () => s.replaceReducer(store))
+      hotRender(App)
+    })
+  }
+
+} else {
+  render(App, rootEl)
+}
