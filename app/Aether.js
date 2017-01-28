b7d4fc8e15847c0de390144733dfd7034053652b
awitherow <au.witherow@gmail.com>
2017-01-28 21:06:28 +0100

    refactor(Aether): organize react router structure

--- app/Aether.js
+++ app/Aether.js
@@ -1,17 +1,57 @@
 import React, { Component, PropTypes } from 'react'
+import { BrowserRouter, Match, Redirect } from 'react-router'
+import { connect } from 'react-redux'
 
-class Aether extends Component{
+import { isUserAuthenticated } from './api/security'
+
+import CMS from './containers/CMS'
+import Portal from './containers/CMS'
+
+class Aether extends Component {
+
   render() {
     return (
-      <div className="aether">
-        {this.props.children}
-      </div>
+      <BrowserRouter>
+        <div id="aether">
+          <MatchWhenAuthorized pattern="/" component={CMS} />
+          <Match pattern="/portal" component={Portal} />
+        </div>
+      </BrowserRouter>
     )
   }
 }
 
-Aether.propTypes = {
-  children: PropTypes.element.isRequired,
+const MatchWhenAuthorized = ({ component, ...rest }) => {
+  console.log(rest)
+  return (
+    <Match {...rest} render={props => {
+      console.log(props, rest)
+      return (
+        isUserAuthenticated() ? (
+          <component {...props} />
+        ) : (
+          <Redirect to={{
+            pathname: '/#/portal',
+            state: { from: props.location },
+          }}
+          />
+        )
+      )}}/>
+  )
 }
 
-export default Aether
+Aether.propTypes = {
+  authenticated: PropTypes.bool.isRequired,
+  user: PropTypes.string.isRequired,
+}
+
+const mapStateToProps = ({
+  authenticated, user,
+}) => ({
+  authenticated,
+  user,
+})
+
+const ConnectedAether = connect(mapStateToProps, null)(Aether)
+
+export default ConnectedAether
--- app/index.js
+++ app/index.js
@@ -1,13 +1,12 @@
 import './lib/styles.css'
-import React, { Component } from 'react'
+import React from 'react'
 import { render } from 'react-dom'
 
 import { Provider } from 'react-redux'
 import { createStore } from 'redux'
 import store from './redux/store'
 
-import { Router, hashHistory } from 'react-router'
-import routes from './routes'
+import Aether from './Aether'
 
 const rootEl = document.getElementById('app')
 
@@ -17,12 +16,6 @@ let s = createStore(store,
     window.__REDUX_DEVTOOLS_EXTENSION__()
   ) : null
 )
-
-class App extends Component {
-  render() {
-    return <Router history={hashHistory} routes={routes} />
-  }
-}
 
 if (process.env.NODE_ENV === 'development') {
   const { AppContainer } = require('react-hot-loader')
@@ -37,15 +30,15 @@ if (process.env.NODE_ENV === 'development') {
     )
   }
 
-  hotRender(App)
+  hotRender(Aether)
 
   if (module.hot) {
     module.hot.accept('./Aether', () => {
       module.hot.accept('./redux/store', () => s.replaceReducer(store))
-      hotRender(App)
+      hotRender(Aether)
     })
   }
 
 } else {
-  render(App, rootEl)
+  render(Aether, rootEl)
 }
