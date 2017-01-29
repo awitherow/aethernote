cb48deaf14200672cfa6473a0ac7a3e1a781c7d7
awitherow <au.witherow@gmail.com>
2017-01-29 11:32:44 +0100

    fix(App): use accepted Hot Loader syntax

--- app/index.js
+++ app/index.js
@@ -17,28 +17,24 @@ let s = createStore(store,
   ) : null
 )
 
-if (process.env.NODE_ENV === 'development') {
-  const { AppContainer } = require('react-hot-loader')
-  const hotRender = (Component) => {
-    render(
-      <AppContainer>
-        <Provider store={s}>
-          <Component />
-        </Provider>
-      </AppContainer>,
-      rootEl,
-    )
-  }
+const { AppContainer } = require('react-hot-loader')
+const hotRender = (Component) => {
+  render(
+    <AppContainer>
+      <Provider store={s}>
+        <Component />
+      </Provider>
+    </AppContainer>,
+    rootEl,
+  )
+}
 
-  hotRender(Aether)
+hotRender(Aether)
 
-  if (module.hot) {
-    module.hot.accept('./Aether', () => {
-      module.hot.accept('./redux/store', () => s.replaceReducer(store))
-      hotRender(Aether)
-    })
-  }
-
-} else {
-  render(Aether, rootEl)
+if (module.hot) {
+  module.hot.accept('./Aether', () => {
+    const NewRoot = require('./Aether').default
+    module.hot.accept('./redux/store', () => s.replaceReducer(store))
+    hotRender(NewRoot)
+  })
 }
